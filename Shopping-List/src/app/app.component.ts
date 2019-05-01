9aafd2bbcdd767b4695ee9320ccb9739c6f46c72
jhonatanAlanFerreira <jhonatanalanb@gmail.com>
2019-05-01 18:24:24 -0300

    Input

--- Shopping-List/src/app/app.component.ts
+++ Shopping-List/src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Shopping-List';
   name = "Jhonatan";
+
+  typed(event){
+    console.log(<HTMLInputElement>event.target.value);
+  }
 }
