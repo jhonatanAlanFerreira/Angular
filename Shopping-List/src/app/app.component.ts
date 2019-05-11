aeb82352abd49e7bb59bd206fe897e787efffb26
jhonatanAlanFerreira <jhonatanalanb@gmail.com>
2019-05-11 12:54:56 -0300

    Edited

--- Shopping-List/src/app/app.component.ts
+++ Shopping-List/src/app/app.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Shopping-List';
   name;
+  tamanhoTela: any;
   
   getValue(value){
     this.name = value;
     console.log("Element",this.name);
+  }
+
+  @HostListener ('window:resize', ['$event'])
+  onResize(event) {
+    this.tamanhoTela = window.innerWidth;
+  }
+
+  ngOnInit(){
+    this.tamanhoTela = window.innerWidth;
   }
 
 }
