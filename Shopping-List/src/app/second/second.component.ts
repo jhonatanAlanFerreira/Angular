c074ce023a9bd22f551a606ddff995bf97d98a99
jhonatanAlanFerreira <jhonatanalanb@gmail.com>
2019-05-11 12:13:28 -0300

    Edited

--- Shopping-List/src/app/second/second.component.ts
+++ Shopping-List/src/app/second/second.component.ts
@@ -9,8 +9,10 @@ export class SecondComponent implements OnInit {
 
   constructor() { }
 
+  nome;
+
   @Input() set call(num){
-    if(num > 0) console.log('Recebido!',num);
+    if(num > 0) {console.log('Recebido!',num);this.nome = "Testando.........";}
   }
 
   ngOnInit() {
