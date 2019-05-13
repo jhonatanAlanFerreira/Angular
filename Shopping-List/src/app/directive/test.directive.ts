ed8e82c8fe5eb3f5c48b64028b0647d024f6f005
jhonatanAlanFerreira <jhonatanalanb@gmail.com>
2019-05-13 01:43:49 -0300

    Edited

--- Shopping-List/src/app/directive/test.directive.ts
+++ Shopping-List/src/app/directive/test.directive.ts
@@ -1,6 +1,5 @@
-import { Directive, OnInit, Renderer2, ElementRef, Input, HostListener, AfterViewChecked, AfterContentInit } from '@angular/core';
+import { Directive, OnInit, Renderer2, ElementRef, Input, HostListener, AfterViewChecked, AfterContentInit, HostBinding } from '@angular/core';
 import { Style } from './style.model';
-import { element } from '@angular/core/src/render3';
 
 @Directive({
   selector: '[appEstilos]'
@@ -10,15 +9,21 @@ export class TestDirective implements OnInit, AfterContentInit {
   style:Style[] = [];
   @Input('appEstilos') estilos:[];
 
+  //HostBinding gets the element on which it was declared
+  @HostBinding('style.backgroundColor') color:string = 'grey';
+
+
   // mouseenter is already known by Angular
   @HostListener('mouseenter') mouseHover() {
     this.styling(new Style("color","red"));
+    this.color = 'white';
   }
 
   // filtering by key and geting the last element
   @HostListener('mouseleave') mouseOut(){
     let color = this.style.filter(element => element.style == "color");
     this.styling(color.slice(-1)[0]);
+    this.color = 'grey';
   }
 
   // elRef gets the element on which it was declared and element will be used by setStyle
