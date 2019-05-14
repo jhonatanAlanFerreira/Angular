8f130af2a557d75a547d873db2157bc4fec41891
jhonatanAlanFerreira <jhonatanalanb@gmail.com>
2019-05-13 23:07:00 -0300

    Edited

---
+++ Shopping-List/src/app/unless.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, TemplateRef, ViewContainerRef, Input, HostBinding } from '@angular/core';
+
+@Directive({
+  selector: '[appUnless]'
+})
+export class UnlessDirective {
+  @Input() set appUnless(condition: boolean){
+    if(!condition) this.vcRef.createEmbeddedView(this.template);
+    else this.vcRef.clear();
+  }
+
+  constructor(private template: TemplateRef<any>, private vcRef: ViewContainerRef) { }
+
+}
