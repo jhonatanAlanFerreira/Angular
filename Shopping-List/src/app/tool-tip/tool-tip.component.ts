62ceb69e1ed557afe36b18d8a00072de0e19221c
jhonatanAlanFerreira <jhonatanalanb@gmail.com>
2019-05-19 16:07:36 -0300

    Edited

--- Shopping-List/src/app/tool-tip/tool-tip.component.ts
+++ Shopping-List/src/app/tool-tip/tool-tip.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2, Input } from '@angular/core';
+import { element } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-tool-tip',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tool-tip.component.css']
 })
 export class ToolTipComponent implements OnInit {
+  @ViewChild('tooltiptext') element:ElementRef;
+  @ViewChild('square') square:ElementRef;
 
-  constructor() { }
+  @Input('corFundo') background = "grey";
+  @Input('corLetra') textcolor = "white";
+
+  constructor(private renderer:Renderer2) { }
 
   ngOnInit() {
+    this.renderer.setStyle(this.element.nativeElement,"backgroundColor",this.background);
+    this.renderer.setStyle(this.square.nativeElement,"borderColor",this.background+" transparent transparent transparent");
+
+    this.renderer.setStyle(this.element.nativeElement,"color",this.textcolor);
   }
 
 }
